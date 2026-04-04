13770041ffeaae1aa48ac8943c06deee17df6939
Renato Rak <88338561+renatorak20@users.noreply.github.com>
2026-04-04 19:20:15 +0200

    api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-export const API_BASE_URL = process.env.JWT_SECRET || "http://localhost:5002/api";
+export const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5002/api";
